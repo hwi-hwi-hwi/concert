0523be1fe9257b311f84f97b485995e51c63e6c1
Park Seohyun <165402279+xeohyun@users.noreply.github.com>
2025-01-17 13:47:39 +0900

    reset

--- src/main/java/com/example/concerts/concert/ConcertRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.concerts.concert;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConcertRepository extends JpaRepository<Concert, Long> {
-}
